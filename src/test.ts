63cf5ca647c83acc68890fc4c277c7d92a62e7d7
nasum <tomato.wonder.life@gmail.com>
2019-07-09 00:55:00 +0900

    first commit

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+import { sampleText, sampleFunction } from './sample'
+
+export function test1() {
+  return "test1"
+}
+
+export function test2() {
+  return { value: "test2"}
+}
+
+let nullAble = null
+nullAble = 'string'
+
+const a = sampleText
+const b = sampleFunction()
